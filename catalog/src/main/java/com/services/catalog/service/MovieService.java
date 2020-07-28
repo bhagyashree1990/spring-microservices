8061b4620e47ec8ea15542e9aeb23a750685efe0
Bhagyashree Chavan <bhagyashree.chavan@sts.in>
2020-07-28 16:55:49 +0530

    8 Implement BulkHead Pattern

--- catalog/src/main/java/com/services/catalog/service/MovieService.java
+++ catalog/src/main/java/com/services/catalog/service/MovieService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.services.catalog.model.CatalogItem;
 import com.services.catalog.model.Movie;
 import com.services.catalog.model.MovieRating;
@@ -17,7 +18,12 @@ public class MovieService {
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem",
+			threadPoolKey = "movieInfoPool",
+			threadPoolProperties = {
+				@HystrixProperty(name = "coreSize", value = "20"),
+				@HystrixProperty(name = "maxQueueSize", value = "10")
+			})
 	public CatalogItem getCatalogItem(MovieRating rating) {
 		Movie movie = restTemplate.getForObject("http://movie-info/movies/"+rating.getMovieId(), Movie.class);//localhost:8081
 		return new CatalogItem(movie.getTitle(), movie.getOverview() ,rating.getRating(), new Date());
