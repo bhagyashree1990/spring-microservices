5f20fb6cc189f33d6f0ccf0de8dae23a5e127ac2
Bhagyashree Chavan <bhagyashree.chavan@sts.in>
2020-07-29 19:30:45 +0530

    11 Set Up Spring Cloud Config Client

--- movie/src/main/java/com/services/movie/controller/HomeController.java
+++ movie/src/main/java/com/services/movie/controller/HomeController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.services.movie.config.MyProperties;
 
 @RestController
+@RefreshScope
 public class HomeController {
 
 	@Autowired
